0c9aa4693360263d04077bca4dbcebc1e149563b
Foguinel <foguinel@gmail.com>
2020-03-18 02:04:28 +0000

    Ping command

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const configBot = require("./configBot.json");
+
+client.on('message', message => {
+    if(message.author.bot) return;
+    if(!message.content.startsWith(configBot.prefix)) return;
+    let args = message.content.split(" ").slice(1);
+    let cmd = message.content.split(" ")[0];
+    cmd = cmd.slice(configBot.prefix.length);
+    try{
+    let exec = require('./commands/' + cmd + '.js');
+    exec.run(client, message, args, botavatar, firebase, database, ms);
+    }catch(erro) { console.log(erro) }
+});
+
+client.on("message", async message => {
+  if(message.author.bot) return;
+
+  if(message.content.indexOf(configBot.prefix) !== 0) return;
+
+  let sender = message.author;
+  const args = message.content.slice(configBot.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  })
+client.login(configBot.token)
